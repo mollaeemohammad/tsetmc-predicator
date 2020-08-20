23cfc35d3ebef5f00a14b30d436481f47e9faf48
Unknown <mollaeemohammad@yahoo.com>
2020-08-20 11:04:39 +0430

    text html scrapping is first solution that was not successful

--- WebScraper.py
+++ WebScraper.py
@@ -5,7 +5,7 @@
 
 from difflib import SequenceMatcher
 import pandas as pd
-from urllib import *
+from urllib.request import urlopen
 
 
 def similar(str1, str2):
@@ -34,5 +34,48 @@ def addSaham(name, url):
     newRow = name + ',' + url
     urlDataset.write(newRow)
     urlDataset.close()
+
+
+def makeNumber(number):
+    number = list(number)
+    length = len(number)
+    delList = []
+    count = 0
+    for i in range(length):
+        if number[i] == '٬':
+            delList.append(i)
+    for i in delList:
+        number.pop(i-count)
+        count += 1
+    return int(''.join(number))
+
+
+#['<FIRST>', '<CLOSE>', '<OPEN>(Yesterday close)', '<HIGH>', '<LOW>', '<VOL>'])
+def getInfo(url):
+    page = urlopen(url)
+    html_bytes = page.read()
+    html = html_bytes.decode("utf-8")
+    # find first price
+    # <span class="open" dir="ltr">۸٬۵۸۸</span>
+    first_index = html.find('<span class="open" dir="ltr">')
+    print(first_index)
+    length = len('<span class="open" dir="ltr">')
+    print(html[first_index: length])
+    first_index = first_index + length
+    end_index = html[first_index-1:].find('</span>') + first_index
+    #FRIST = makeNumber(html[first_index, end_index])
+    print(first_index, end_index)
+    #print(html[first_index: end_index])
+    # find open price
+    # <span class dir="ltr">۸٬۸۵۳</span>
+    #open_index = html.find('')
+    
+
+
+
+
+
+
+
 
 
